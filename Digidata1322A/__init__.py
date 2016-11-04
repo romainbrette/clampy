02dd6b6dcbd299ca3c62ddd3f279ae11c5aeffa0
Romain <romain.brette@inserm.fr>
2016-11-04 17:33:19 +0100

    Some info on digidata code

--- Digidata1322A/__init__.py
+++ Digidata1322A/__init__.py
@@ -1,1 +1,7 @@
 from DigiData import *
+
+"""
+This is taken from Pyme.
+Check here:
+https://bitbucket.org/david_baddeley/python-microscopy/src/60c9379235f90b2c96afcf735099c69958563479/PYME/Acquire/Hardware/DigiData/?at=default
+"""
