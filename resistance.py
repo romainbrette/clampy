87f5bc7bb6a231bd7b0243854e03562245610cd6
Romain <romain.brette@inserm.fr>
2016-12-16 13:24:37 +0100

    Resistance.py shows values of inputs graphically

--- resistance.py
+++ resistance.py
@@ -6,8 +6,13 @@ from Digidata1322A import *
 from pylab import *
 from time import sleep, time
 import matplotlib.animation as animation
+from Tkinter import *
+
+window = Tk()
 
 board = DigiData()
+
+"""
 #for i in range(100):
 #    print board.GetAIValue(0)
 #print board.hDev
@@ -27,3 +32,17 @@ board.PutAOValue(1, 0)
 sleep(0.2) # there seems to be a 200 ms transient in the response (measured I??)
 for _ in range(20):
     print time(),[board.GetAIValue(i) for i in range(12)]
+"""
+
+scale=[]
+for _ in range(12):
+    scale.append(Scale(window, orient=HORIZONTAL, from_=-32768, to=32767).pack())
+
+def refresh():
+    for i in range(12):
+        scale[i].set(board.GetAIValue(i))
+    window.after(200, refresh)
+
+window.after(200, refresh)
+
+mainloop()
