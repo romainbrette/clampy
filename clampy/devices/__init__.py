e6a9803aa344569fcc722b78937e7aee5b192cf4
Romain <romain.brette@inserm.fr>
2019-02-28 16:28:13 +0100

    Fixed the init of devices: now gives a warning when fails to import Axoclamp.

--- clampy/devices/__init__.py
+++ clampy/devices/__init__.py
@@ -1,3 +1,11 @@
+from warnings import warn
 from .ni import *
-from .multiclamp import *
-from .axoclamp900A import *
+# Maybe these imports should not be automatic
+try:
+    from .multiclamp import *
+except:
+    warn('Failed to import the Multiclamp module')
+try:
+    from .axoclamp900A import *
+except:
+    warn('Failed to import the Axoclamp 900A module')
